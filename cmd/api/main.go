7f639ae47dae4f9c797a3809cf1190dfed27e24f
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-05 18:28:08 +0300

    :sparkles: adding routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
+type application struct{}
+
 func main() {
-	http.HandleFunc("/v1/health-check", HealthCheckHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	app := &application{}
+	log.Fatal(http.ListenAndServe(":8080", app.routes()))
 }
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("works"))
+}
+
+func (a *application) routes() http.Handler {
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/health-check", HealthCheckHandler)
+	return router
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/3n0ugh/BasedWeb
 
 go 1.17
+
+require github.com/julienschmidt/httprouter v1.3.0
