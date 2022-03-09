3b7c1a98ebe3d46ec9e60efef13a9ba4a057ffb4
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-09 09:01:03 +0300

    :sparkles: add test create blog handler

--- cmd/api/handler_test.go
+++ cmd/api/handler_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/3n0ugh/BasedWeb/internal/data"
+	"github.com/3n0ugh/BasedWeb/internal/data/mock"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestHealthCheckHandler(t *testing.T) {
@@ -47,4 +52,69 @@ func TestHealthCheckHandler(t *testing.T) {
 			t.Errorf("body -> want: %q; got: %q", test.wantBody, body)
 		}
 	})
+}
+
+func TestCreateBlogHandler(t *testing.T) {
+	app := &application{
+		model: mock.NewModel(),
+	}
+
+	reqBody := data.Blog{
+		Title:    "gRPC in Go!",
+		Body:     "I do not know yet",
+		Category: []string{"Golang", "Network"},
+	}
+
+	var blog = data.Blog{
+		ID:        11,
+		CreatedAt: time.Now(),
+		Title:     "gRPC in Go!",
+		Body:      "I do not know yet",
+		Category:  []string{"Golang", "Network"},
+		Version:   3,
+	}
+
+	wantBody, err := app.prettyJSON(envelope{"blog": blog})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wantUrl := "/v1/blogs"
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		// TODO: add some test cases after the write validation for blog
+		{name: "Should Success", urlPath: wantUrl, wantCode: 201, wantBody: wantBody},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reqBodyJSON, err := json.Marshal(reqBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodPost, tt.urlPath, strings.NewReader(string(reqBodyJSON)))
+
+			app.createBlogHandler(w, r)
+
+			responseBody, err := ioutil.ReadAll(w.Result().Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if tt.wantCode != w.Result().StatusCode {
+				t.Errorf("Status Code -> want: %d; got: %d", tt.wantCode, w.Result().StatusCode)
+			}
+
+			if !reflect.DeepEqual(tt.wantBody, responseBody) {
+				t.Errorf("Response Body -> want: \n%q; got: \n%q", tt.wantBody, responseBody)
+			}
+		})
+	}
 }
