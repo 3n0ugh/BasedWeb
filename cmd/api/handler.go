c9b8317d33b1efdaf68338f88b2d3c7a661aa6e3
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-11 09:08:19 +0300

    :sparkles: add update blog handler

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -107,3 +107,64 @@ func (app *application) deleteBlogHandler(w http.ResponseWriter, r *http.Request
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) updateBlogHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readParamID(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	blog, err := app.model.Blog.Get(id)
+	if err != nil {
+		if errors.Is(data.ErrRecordNotFound, err) {
+			app.notFoundResponse(w, r)
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	var input struct {
+		Title    *string  `json:"title"`
+		Body     *string  `json:"body"`
+		Category []string `json:"category"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if input.Title != nil {
+		blog.Title = *input.Title
+	}
+
+	if input.Body != nil {
+		blog.Body = *input.Body
+	}
+
+	if input.Category != nil {
+		blog.Category = input.Category
+	}
+
+	v := validator.New()
+
+	if data.ValidateBlog(v, blog); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.model.Blog.Update(blog)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"blog": blog}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
