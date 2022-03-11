5f6c446d071d1f56212ac80812b96912226f0bcf
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-11 09:10:28 +0300

    :sparkles: add update blog handler into router

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -13,6 +13,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/blogs", app.createBlogHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/blogs/:id", app.showBlogHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/blogs/:id", app.deleteBlogHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/blogs/:id", app.updateBlogHandler)
 
 	return router
 }
