ca34f265fce2aeee8a11f37d039ae43e8550150f
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-12 19:58:47 +0300

    :sparkles: update blog model interface

--- internal/data/model.go
+++ internal/data/model.go
@@ -16,6 +16,7 @@ type Model struct {
 		Get(id int64) (*Blog, error)
 		Update(blog *Blog) error
 		Delete(id int64) error
+		GetAll(title string, category []string, f Filter) ([]*Blog, Metadata, error)
 	}
 }
 
