e36004e946ec258f21538d87aac05b4774e2d7f4
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-12 12:28:20 +0300

    :sparkles: add rate limitter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/tomasen/realip"
+	"golang.org/x/time/rate"
+)
 
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +21,64 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	})
 }
 
-// TODO: Add Rate Limitter
+func (app *application) rateLimit(next http.Handler) http.Handler {
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	// Declare a mutex and a map to hold the clients' IP addresses and rate limiters.
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.config.limiter.enabled {
+
+			// Extract the client's IP address from the request.
+			ip := realip.FromRequest(r)
+
+			mu.Lock()
+
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(
+						rate.Limit(app.config.limiter.rps),
+						app.config.limiter.burst,
+					),
+				}
+			}
+
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
+			mu.Unlock()
+
+		}
+		next.ServeHTTP(w, r)
+	})
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.17
 require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.4
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65
 )
