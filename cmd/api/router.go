1477eaf7f90b7ab3762866d66c4bb350f077294c
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-12 12:09:58 +0300

    :sparkles: add router custom error messages

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -8,6 +8,9 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/health-check", app.HealthCheckHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/blogs", app.createBlogHandler)
