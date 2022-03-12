aa83cadddc0b56a46b617e61c53d2d868664d148
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-12 19:59:26 +0300

    :sparkles: add list blog handler

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -168,3 +168,44 @@ func (app *application) updateBlogHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 }
+
+func (app *application) listBlogHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title    string
+		Category []string
+		data.Filter
+	}
+
+	v := validator.New()
+	query := r.URL.Query()
+
+	input.Title = app.readString(query, "title", "")
+
+	input.Category = app.readCSV(query, "category", []string{})
+
+	input.Filter.Page = app.readInt(query, "page", 1, v)
+	input.Filter.PageSize = app.readInt(query, "page_size", 20, v)
+	input.Filter.Sort = app.readString(query, "sort", "id")
+
+	input.Filter.SortSafeList = []string{"id", "title", "-id", "-title"}
+
+	if data.ValidateFilter(v, input.Filter); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	blogs, metadata, err := app.model.Blog.GetAll(input.Title, input.Category, input.Filter)
+	if err != nil {
+		if errors.Is(err, data.ErrRecordNotFound) {
+			app.notFoundResponse(w, r)
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"blogs": blogs, "metadata": metadata}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
