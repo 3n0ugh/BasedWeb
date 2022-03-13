f3465e50b7619a936e7cca2cc4a7daf56d46d482
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-13 21:17:11 +0300

    :sparkles: add email validation regex

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -2,6 +2,10 @@ package validator
 
 import "regexp"
 
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
 type Validator struct {
 	Errors map[string]string
 }
